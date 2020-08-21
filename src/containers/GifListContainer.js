13d6543ac173faf193b108058dc97c37353963d1
Juan Cobian <cobianjuan19@gmail.com>
2020-08-21 01:05:30 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import GifList from '../components/GifList'
 import GifSearch from '../components/GifSearch'
 
-export default GifListContainer extends Component {
+export default class GifListContainer extends Component {
 
   state = {
     gifs: []
